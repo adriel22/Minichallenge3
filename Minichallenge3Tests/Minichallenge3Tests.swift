37d92c12f13351bb39b22b345c8736aef20f1d58
adriel22 <adriel.arruda@yahoo.com.br>
2019-05-08 09:54:22 -0300

    testing 13

--- Minichallenge3Tests/Minichallenge3Tests.swift
+++ Minichallenge3Tests/Minichallenge3Tests.swift
@@ -23,7 +23,7 @@ class Minichallenge3Tests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        
+        XCTAssert(1 == 2)
     }
 
     func testPerformanceExample() {
