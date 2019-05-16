0baedb11aaf26778a4ea52357481dbbf84bc2ddd
João Pedro <jpedroarag@hotmail.com>
2019-05-15 21:23:07 -0300

    Refactor

--- Minichallenge3/Views/ExpandableCell/ExpandableTableViewHeaderView.swift
+++ Minichallenge3/Views/ExpandableCell/ExpandableTableViewHeaderView.swift
@@ -10,46 +10,54 @@ import UIKit
 
 class ExpandableTableViewHeaderView: UIView {
 
-    var label: UILabel!
-    var button: UIButton!
+    lazy var label: UILabel! = UILabel(frame: .zero)
+    lazy var button: UIButton! = UIButton(frame: .zero)
+    
+    lazy var isCollapsed = true
     var didTap: ((UIButton) -> Void)?
-    var isCollapsed = true
 
-    override init(frame: CGRect) {
+    override init(frame: CGRect = .zero) {
         super.init(frame: frame)
 
-        label = UILabel()
-        label.font = .systemFont(ofSize: 13, weight: .regular)
-        label.textColor = UIColor(rgb: 0x8E8E93)
+        configureLabel()
         addSubview(label)
 
-        button = UIButton()
+        configureButton()
+        addSubview(button)
+        
+    }
+    
+    private func configureLabel() {
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = UIColor(rgb: 0x8E8E93)
+    }
+    
+    private func configureButton() {
         button.setTitleColor(UIColor(color: .darkBlue), for: .normal)
         button.addTarget(self, action: #selector(tapped(_:)), for: .touchUpInside)
-        addSubview(button)
-
+    }
+    
+    private func setConstraints() {
         widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
         label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
         label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
         button.translatesAutoresizingMaskIntoConstraints = false
         button.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
         button.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
         button.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         button.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
-
-    convenience init() {
-        self.init(frame: .zero)
+    
+    @objc private func tapped(_ sender: UIButton) {
+        didTap?(sender)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("Not implemented yet")
-    }
-
-    @objc private func tapped(_ sender: UIButton) {
-        didTap?(sender)
     }
 
 }
