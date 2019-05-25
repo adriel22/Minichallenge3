6c44e1121cdac312f6febf7625828050b9082b63
João Pedro <jpedroarag@hotmail.com>
2019-05-25 13:33:44 -0300

    Added extension for set status bar light in all navigation controllers

--- Minichallenge3/Controllers/MyNarrativesViewController.swift
+++ Minichallenge3/Controllers/MyNarrativesViewController.swift
@@ -26,12 +26,6 @@ class MyNarrativesViewController: UIViewController {
         return tableView
     }()
 
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.barStyle = .black
-    }
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ Minichallenge3/Extensions/UINavigationController+preferredBarStyle.swift
@@ -0,0 +1,15 @@
+//
+//  UINavigationController+preferredBarStyle.swift
+//  Minichallenge3
+//
+//  Created by João Pedro Aragão on 25/05/19.
+//  Copyright © 2019 Adriel Freire. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
